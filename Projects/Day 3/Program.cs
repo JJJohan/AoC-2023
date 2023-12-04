d203c700f91ddabe1fdb8656b4ef0f4f3f2d6ec1
Johan Rensenbrink <admin@renscreations.com>
2023-12-04 15:59:22 +1000

    Day 3 and 4.

---
+++ Projects/Day 3/Program.cs
@@ -0,0 +1,216 @@
+﻿using System.Diagnostics;
+
+static class Program
+{
+    private static bool IsSymbol(char c, out bool isAsterisk)
+    {
+        if (c == '*')
+        {
+            isAsterisk = true;
+            return true;
+        }
+
+        isAsterisk = false;
+        return c != '.' && !char.IsDigit(c);
+    }
+
+    private static bool AdjacentToSymbol(string[] lines, int lineIndex, int index, int length, out (int, int) symbolIndex, out bool isAsterisk)
+    {
+        if (index > 0 && IsSymbol(lines[lineIndex][index - 1], out isAsterisk))
+        {
+            symbolIndex = (lineIndex, index - 1);
+            return true;
+        }
+
+        if (index + length < lines[lineIndex].Length - 1 && IsSymbol(lines[lineIndex][index + length], out isAsterisk))
+        {
+            symbolIndex = (lineIndex, index + length);
+            return true;
+        }
+
+        // Above
+        if (lineIndex > 0)
+        {
+            // Diagonal
+            if (index > 0 && IsSymbol(lines[lineIndex - 1][index - 1], out isAsterisk))
+            {
+                symbolIndex = (lineIndex - 1, index - 1);
+                return true;
+            }
+
+            if (index + length < lines[lineIndex - 1].Length - 1 && IsSymbol(lines[lineIndex - 1][index + length], out isAsterisk))
+            {
+                symbolIndex = (lineIndex - 1, index + length);
+                return true;
+            }
+
+            for (int i = 0; i < length; ++i)
+            {
+                if (IsSymbol(lines[lineIndex - 1][index + i], out isAsterisk))
+                {
+                    symbolIndex = (lineIndex - 1, index + i);
+                    return true;
+                }
+            }
+        }
+
+        // Below
+        if (lineIndex < lines.Length - 1)
+        {
+            // Diagonal
+            if (index > 0 && IsSymbol(lines[lineIndex + 1][index - 1], out isAsterisk))
+            {
+                symbolIndex = (lineIndex + 1, index - 1);
+                return true;
+            }
+            if (index + length < lines[lineIndex + 1].Length - 1 && IsSymbol(lines[lineIndex + 1][index + length], out isAsterisk))
+            {
+                symbolIndex = (lineIndex + 1, index + length);
+                return true;
+            }
+            for (int i = 0; i < length; ++i)
+            {
+                if (IsSymbol(lines[lineIndex + 1][index + i], out isAsterisk))
+                {
+                    symbolIndex = (lineIndex + 1, index + i);
+                    return true;
+                }
+            }
+        }
+
+        isAsterisk = false;
+        symbolIndex = (default, default);
+        return false;
+    }
+
+    public static void Main()
+    {
+        string[] lines = File.ReadAllLines(Path.Combine("..", "Input", "day3.txt"));
+
+        int sum = 0;
+        for (int i = 0; i < lines.Length; ++i)
+        {
+            string line = lines[i];
+            int digitStart = 0;
+            int digitLength = 0;
+
+            for (int j = 0; j < line.Length; ++j)
+            {
+                if (char.IsDigit(line[j]))
+                {
+                    if (digitLength == 0)
+                    {
+                        digitStart = j;
+                        digitLength = 1;
+                    }
+                    else
+                    {
+                        ++digitLength;
+                    }
+                }
+                else
+                {
+                    if (digitLength > 0)
+                    {
+                        if (AdjacentToSymbol(lines, i, digitStart, digitLength, out _, out _))
+                        {
+                            int number = int.Parse(line.Substring(digitStart, digitLength));
+                            sum += number;
+                        }
+                    }
+
+                    digitLength = 0;
+                }
+            }
+
+            if (digitLength > 0)
+            {
+                if (AdjacentToSymbol(lines, i, digitStart, digitLength, out _, out _))
+                {
+                    int number = int.Parse(line.Substring(digitStart, digitLength));
+                    sum += number;
+                }
+            }
+        }
+
+        Console.WriteLine($"Sum: {sum}");
+
+        // Part 2
+
+        Dictionary<(int, int), List<int>> gearValues = new();
+
+        for (int i = 0; i < lines.Length; ++i)
+        {
+            string line = lines[i];
+            int digitStart = 0;
+            int digitLength = 0;
+
+            for (int j = 0; j < line.Length; ++j)
+            {
+                if (char.IsDigit(line[j]))
+                {
+                    if (digitLength == 0)
+                    {
+                        digitStart = j;
+                        digitLength = 1;
+                    }
+                    else
+                    {
+                        ++digitLength;
+                    }
+                }
+                else
+                {
+                    if (digitLength > 0)
+                    {
+                        if (AdjacentToSymbol(lines, i, digitStart, digitLength, out (int, int) symbolIndex, out bool isAsterisk))
+                        {
+                            if (isAsterisk)
+                            {
+                                if (!gearValues.TryGetValue(symbolIndex, out List<int>? values))
+                                {
+                                    values = new();
+                                    gearValues.Add(symbolIndex, values);
+                                }
+
+                                int number = int.Parse(line.Substring(digitStart, digitLength));
+                                values.Add(number);
+                            }
+                        }
+                    }
+
+                    digitLength = 0;
+                }
+            }
+
+            if (digitLength > 0)
+            {
+                if (AdjacentToSymbol(lines, i, digitStart, digitLength, out (int, int) symbolIndex, out bool isAsterisk))
+                {
+                    if (isAsterisk)
+                    {
+                        if (!gearValues.TryGetValue(symbolIndex, out List<int>? values))
+                        {
+                            values = new();
+                            gearValues.Add(symbolIndex, values);
+                        }
+
+                        int number = int.Parse(line.Substring(digitStart, digitLength));
+                        values.Add(number);
+                    }
+                }
+            }
+        }
+
+        int gearValuesSum = 0;
+        foreach (List<int> values in gearValues.Values)
+        {
+            if (values.Count == 2)
+            {
+                gearValuesSum += values[0] * values[1];
+            }
+        }
+
+        Console.WriteLine($"Gear values sum: {gearValuesSum}");
+    }
+}
---
+++ Projects/Day 4/Program.cs
@@ -0,0 +1,86 @@
+﻿static class Program
+{
+    struct Line
+    {
+        public HashSet<int> WinningNumbers;
+        public HashSet<int> OtherNumbers;
+        public int Matches;
+    }
+
+    public static void Main()
+    {
+        string[] lines = File.ReadAllLines(Path.Combine("..", "Input", "day4.txt"));
+        Line[] data = new Line[lines.Length];
+
+        for (int i = 0; i < lines.Length; ++i)
+        {
+            ref Line d = ref data[i];
+            d.WinningNumbers = new();
+            d.OtherNumbers = new();
+            d.Matches = 0;
+
+            string l = lines[i];
+            l = l.Split(':').Last();
+
+            string[] parts = l.Split('|');
+            string winningNumbers = parts[0];
+
+            string[] winningNumberParts = winningNumbers.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            foreach (string number in winningNumberParts)
+            {
+                d.WinningNumbers.Add(int.Parse(number));
+            }
+
+            string otherNumbers = parts[1];
+            string[] otherNumberParts = otherNumbers.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            foreach (string number in otherNumberParts)
+            {
+                d.OtherNumbers.Add(int.Parse(number));
+            }
+        }
+
+        int total = 0;
+        for (int i = 0; i < data.Length; ++i)
+        {
+            ref Line d = ref data[i];
+            int lineValue = 0;
+            foreach (int winningNumber in d.WinningNumbers)
+            {
+                if (d.OtherNumbers.Contains(winningNumber))
+                {
+                    lineValue = lineValue == 0 ? 1 : lineValue * 2;
+                    ++d.Matches;
+                }
+            }
+
+            total += lineValue;
+        }
+
+        Console.WriteLine($"Total: {total}");
+
+        // Part 2
+
+        int totalCards = data.Length;
+        Stack<int> scratchCards = new();
+        for (int i = 0; i < totalCards; ++i)
+        {
+            scratchCards.Push(i);
+        }
+
+        while (scratchCards.Count > 0)
+        {
+            int index = scratchCards.Pop();
+            ref Line card = ref data[index];
+
+            for (int i = 0; i < card.Matches; ++i)
+            {
+                int newIndex = index + i + 1;
+                if (newIndex >= data.Length) break;
+                scratchCards.Push(newIndex);
+                ++totalCards;
+            }
+        }
+
+        Console.WriteLine($"Total scratch cards: {totalCards}");
+    }
+}
