8b367c796203c11e1d49d0f0cec43a6eb6e6d378
Johan Rensenbrink <admin@renscreations.com>
2023-12-18 17:54:49 +1000

    Day 17 and 18.

---
+++ Projects/Day 17/Program.cs
@@ -0,0 +1,243 @@
+﻿using System.Text;
+
+static class Program
+{
+    private const bool PrintToConsole = false;
+
+    private enum Direction : byte
+    {
+        Right,
+        Left,
+        Up,
+        Down
+    }
+    private struct Tile
+    {
+        public byte X;
+        public byte Y;
+        public Direction Direction;
+        public byte RepeatCount;
+        public ushort HeatLoss;
+        public Dictionary<(int, int), Direction> History;
+
+        public Tile(Dictionary<(int, int), Direction> history, byte x, byte y, Direction direction, byte repeatCount, ushort heatLoss)
+        {
+            X = x;
+            Y = y;
+            Direction = direction;
+            RepeatCount = repeatCount;
+            HeatLoss = heatLoss;
+            History = history;
+        }
+    }
+
+    private static int rows;
+    private static int cols;
+    private static (byte X, byte Y) end;
+    private static ushort[,] heatValues = new ushort[0, 0];
+
+    private static void EnqueueTile(in Tile current, byte repeatCount, Direction direction, PriorityQueue<Tile, ushort> queue,
+        Dictionary<(byte, byte, Direction, byte), ushort> cache)
+    {
+        byte x = (byte)(direction == Direction.Left ? current.X - 1 : direction == Direction.Right ? current.X + 1 : current.X);
+        byte y = (byte)(direction == Direction.Down ? current.Y + 1 : direction == Direction.Up ? current.Y - 1 : current.Y);
+        (byte, byte, Direction, byte) cacheKey = (x, y, direction, repeatCount);
+        ushort newCost = (ushort)(current.HeatLoss + heatValues[x, y]);
+
+        if (cache.TryGetValue(cacheKey, out ushort lowestHeat))
+        {
+            if (newCost >= lowestHeat) return;
+
+            cache[cacheKey] = newCost;
+        }
+        else
+        {
+            cache.Add(cacheKey, newCost);
+        }
+
+        Dictionary<(int, int), Direction> newHistory = new(current.History);
+        newHistory.TryAdd((x, y), direction);
+        queue.Enqueue(new(newHistory, x, y, direction, repeatCount, newCost), (ushort)(Math.Abs(x - end.X) + Math.Abs(y - end.Y) + newCost));
+    }
+
+    private static void PrintShortestPath(in Tile path)
+    {
+        StringBuilder sb = new();
+        for (int y = 0; y < rows; ++y)
+        {
+            for (int x = 0; x < cols; ++x)
+            {
+                if (path.History.TryGetValue((x, y), out Direction dir))
+                {
+                    sb.Append("\x1b[91m");
+                    if (dir == Direction.Left)
+                        sb.Append('<');
+                    else if (dir == Direction.Right)
+                        sb.Append('>');
+                    else if (dir == Direction.Up)
+                        sb.Append('^');
+                    else
+                        sb.Append('V');
+                    sb.Append("\x1b[39m");
+                }
+                else
+                {
+                    sb.Append(heatValues[x, y]);
+                }
+            }
+            sb.AppendLine();
+        }
+
+        Console.WriteLine(sb.ToString());
+    }
+
+    public static void Main()
+    {
+        string[] lines = File.ReadAllLines(Path.Combine("..", "Input", "day17.txt"));
+
+        rows = lines.Length;
+        cols = lines[0].Length;
+
+        heatValues = new ushort[cols, rows];
+        for (int i = 0; i < lines.Length; ++i)
+        {
+            string line = lines[i];
+            ushort[] numbers = line.Select(c => (ushort)(c - '0')).ToArray();
+            for (int j = 0; j < cols; ++j)
+                heatValues[j, i] = numbers[j];
+        }
+
+        end = ((byte)(cols - 1), (byte)(rows - 1));
+
+        Tile start1 = new(new() { { (1, 0), Direction.Right } }, 1, 0, Direction.Right, 1, heatValues[1, 0]);
+        Tile start2 = new(new() { { (0, 1), Direction.Down } }, 0, 1, Direction.Down, 1, heatValues[0, 1]);
+
+        PriorityQueue<Tile, ushort> activeTiles = new();
+        activeTiles.Enqueue(start1, 0);
+        activeTiles.Enqueue(start2, 0);
+        Dictionary<(byte, byte, Direction, byte), ushort> visitedTiles = new();
+
+        Tile shortestPath = new();
+        int lowestHeatLoss = int.MaxValue;
+        while (activeTiles.Count > 0)
+        {
+            Tile current = activeTiles.Dequeue();
+
+            if (current.X == end.X && current.Y == end.Y && current.HeatLoss < lowestHeatLoss)
+            {
+                shortestPath = current;
+                lowestHeatLoss = current.HeatLoss;
+                break;
+            }
+
+            if (current.Direction == Direction.Left)
+            {
+                if (current.RepeatCount < 2 && current.X > 0)
+                    EnqueueTile(current, (byte)(current.RepeatCount + 1), Direction.Left, activeTiles, visitedTiles);
+                if (current.Y > 0)
+                    EnqueueTile(current, 0, Direction.Up, activeTiles, visitedTiles);
+                if (current.Y < rows - 1)
+                    EnqueueTile(current, 0, Direction.Down, activeTiles, visitedTiles);
+            }
+            else if (current.Direction == Direction.Right)
+            {
+                if (current.RepeatCount < 2 && current.X < cols - 1)
+                    EnqueueTile(current, (byte)(current.RepeatCount + 1), Direction.Right, activeTiles, visitedTiles);
+                if (current.Y > 0)
+                    EnqueueTile(current, 0, Direction.Up, activeTiles, visitedTiles);
+                if (current.Y < rows - 1)
+                    EnqueueTile(current, 0, Direction.Down, activeTiles, visitedTiles);
+            }
+            else if (current.Direction == Direction.Up)
+            {
+                if (current.RepeatCount < 2 && current.Y > 0)
+                    EnqueueTile(current, (byte)(current.RepeatCount + 1), Direction.Up, activeTiles, visitedTiles);
+                if (current.X > 0)
+                    EnqueueTile(current, 0, Direction.Left, activeTiles, visitedTiles);
+                if (current.X < cols - 1)
+                    EnqueueTile(current, 0, Direction.Right, activeTiles, visitedTiles);
+            }
+            else // Down
+            {
+                if (current.RepeatCount < 2 && current.Y < rows - 1)
+                    EnqueueTile(current, (byte)(current.RepeatCount + 1), Direction.Down, activeTiles, visitedTiles);
+                if (current.X > 0)
+                    EnqueueTile(current, 0, Direction.Left, activeTiles, visitedTiles);
+                if (current.X < cols - 1)
+                    EnqueueTile(current, 0, Direction.Right, activeTiles, visitedTiles);
+            }
+        }
+
+        PrintShortestPath(shortestPath);
+
+        Console.WriteLine($"Lowest heat loss: {lowestHeatLoss}");
+
+        // Part 2
+
+        activeTiles.Clear();
+        start1 = new(new() { { (1, 0), Direction.Right } }, 1, 0, Direction.Right, 0, heatValues[1, 0]);   
+        start2 = new(new() { { (0, 1), Direction.Down } }, 0, 1, Direction.Down, 0, heatValues[0, 1]);
+        activeTiles.Enqueue(start1, 0);
+        activeTiles.Enqueue(start2, 0);
+        visitedTiles.Clear();
+
+        shortestPath = new();
+        lowestHeatLoss = int.MaxValue;
+        while (activeTiles.Count > 0)
+        {
+            Tile current = activeTiles.Dequeue();
+
+            if (current.X == end.X && current.Y == end.Y && current.HeatLoss < lowestHeatLoss && current.RepeatCount >= 3)
+            {
+                shortestPath = current;
+                lowestHeatLoss = current.HeatLoss;
+                Console.WriteLine($"New entry: {lowestHeatLoss}");
+                continue;
+            }
+
+            if (current.Direction == Direction.Left)
+            {
+                if (current.RepeatCount < 9 && current.X > 0)
+                    EnqueueTile(current, (byte)(current.RepeatCount + 1), Direction.Left, activeTiles, visitedTiles);
+                if (current.RepeatCount >= 3 && current.Y > 0)
+                    EnqueueTile(current, 0, Direction.Up, activeTiles, visitedTiles);
+                if (current.RepeatCount >= 3 && current.Y < rows - 1)
+                    EnqueueTile(current, 0, Direction.Down, activeTiles, visitedTiles);
+            }
+            else if (current.Direction == Direction.Right)
+            {
+                if (current.RepeatCount < 9 && current.X < cols - 1)
+                    EnqueueTile(current, (byte)(current.RepeatCount + 1), Direction.Right, activeTiles, visitedTiles);
+                if (current.RepeatCount >= 3 && current.Y > 0)
+                    EnqueueTile(current, 0, Direction.Up, activeTiles, visitedTiles);
+                if (current.RepeatCount >= 3 && current.Y < rows - 1)
+                    EnqueueTile(current, 0, Direction.Down, activeTiles, visitedTiles);
+            }
+            else if (current.Direction == Direction.Up)
+            {
+                if (current.RepeatCount < 9 && current.Y > 0)
+                    EnqueueTile(current, (byte)(current.RepeatCount + 1), Direction.Up, activeTiles, visitedTiles);
+                if (current.RepeatCount >= 3 && current.X > 0)
+                    EnqueueTile(current, 0, Direction.Left, activeTiles, visitedTiles);
+                if (current.RepeatCount >= 3 && current.X < cols - 1)
+                    EnqueueTile(current, 0, Direction.Right, activeTiles, visitedTiles);
+            }
+            else // Down
+            {
+                if (current.RepeatCount < 9 && current.Y < rows - 1)
+                    EnqueueTile(current, (byte)(current.RepeatCount + 1), Direction.Down, activeTiles, visitedTiles);
+                if (current.RepeatCount >= 3 && current.X > 0)
+                    EnqueueTile(current, 0, Direction.Left, activeTiles, visitedTiles);
+                if (current.RepeatCount >= 3 && current.X < cols - 1)
+                    EnqueueTile(current, 0, Direction.Right, activeTiles, visitedTiles);
+            }
+        }
+
+        if (PrintToConsole)
+        {
+            PrintShortestPath(shortestPath);
+        }
+
+        Console.WriteLine($"Lowest heat loss: {lowestHeatLoss}");
+    }
+}
---
+++ Projects/Day 18/Program.cs
@@ -0,0 +1,183 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+
+static class Program
+{
+    private struct Data
+    {
+        public char Direction;
+        public int Metres;
+        public string ColorCode;
+    }
+
+    private static bool IsPointInPolygon(List<(int X, int Y)> holesDug, (int X, int Y) node)
+    {
+        bool result = false;
+        int j = holesDug.Count - 1;
+        for (int i = 0; i < holesDug.Count; i++)
+        {
+            if (holesDug[i].Y < node.Y && holesDug[j].Y >= node.Y ||
+                holesDug[j].Y < node.Y && holesDug[i].Y >= node.Y)
+            {
+                if (holesDug[i].X + (node.Y - holesDug[i].Y) /
+                   (holesDug[j].Y - holesDug[i].Y) *
+                   (holesDug[j].X - holesDug[i].X) < node.X)
+                {
+                    result = !result;
+                }
+            }
+            j = i;
+        }
+        return result;
+    }
+
+    public static void Main()
+    {
+        string[] lines = File.ReadAllLines(Path.Combine("..", "Input", "day18.txt"));
+
+        Data[] data = new Data[lines.Length];
+        for (int i = 0; i < lines.Length; ++i)
+        {
+            string line = lines[i];
+            string[] parts = line.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            Debug.Assert(parts.Length == 3);
+
+            data[i] = new Data
+            {
+                Direction = parts[0][0],
+                Metres = int.Parse(parts[1]),
+                ColorCode = parts[2]
+            };
+        }
+
+        List<(int X, int Y)> holesDug = new();
+        (int X, int Y) current = (0, 0);
+        holesDug.Add(current);
+
+        int maxX = 0;
+        int maxY = 0;
+        int minX = int.MaxValue;
+        int minY = int.MaxValue;
+
+        for (int i = 0; i < data.Length; ++i)
+        {
+            ref Data d = ref data[i];
+            for (int j = 0; j < d.Metres; ++j)
+            {
+                if (d.Direction == 'D')
+                    current.Y += 1;
+                else if (d.Direction == 'U')
+                    current.Y -= 1;
+                else if (d.Direction == 'L')
+                    current.X -= 1;
+                else if (d.Direction == 'R')
+                    current.X += 1;
+                else
+                    Debug.Fail("Unexpected direction.");
+
+                minX = Math.Min(current.X, minX);
+                minY = Math.Min(current.Y, minY);
+                maxX = Math.Max(current.X, maxX);
+                maxY = Math.Max(current.Y, maxY);
+                holesDug.Add(current);
+            }
+        }
+
+        long holeArea = 0;
+        HashSet<(int, int)> boundary = new();
+        for (int i = 0; i < holesDug.Count; ++i)
+        {
+            boundary.Add((holesDug[i].Item1, holesDug[i].Item2));
+        }
+
+        for (int y = minY; y <= maxY; ++y)
+        {
+            for (int x = minX; x <= maxX; ++x)
+            {
+                if (boundary.Contains((x, y)) || IsPointInPolygon(holesDug, (x, y)))
+                {
+                    ++holeArea;
+                }
+            }
+        }
+
+        Console.WriteLine($"Hole area: {holeArea}");
+
+        // Part 2
+
+        holesDug.Clear();
+        current = (0, 0);
+        holesDug.Add(current);
+
+        // 'fix' the data.
+        for (int i = 0; i < data.Length; ++i)
+        {
+            ref Data d = ref data[i];
+            int metres = int.Parse(d.ColorCode.Substring(2, 5), NumberStyles.HexNumber);
+            int dir = int.Parse(d.ColorCode.Substring(7, 1), NumberStyles.HexNumber);
+
+            for (int j = 0; j < metres; ++j)
+            {
+                if (dir == 1) // Down
+                    current.Y += 1;
+                else if (dir == 3) // Up
+                    current.Y -= 1;
+                else if (dir == 2) // Left
+                    current.X -= 1;
+                else if (dir == 0) // Right
+                    current.X += 1;
+                else
+                    Debug.Fail("Unexpected direction.");
+
+                holesDug.Add(current);
+            }
+        }
+
+        // Calculate the number of lattice points on the boundary
+        long boundaryPoints = 0;
+        for (int i = 0; i < holesDug.Count; ++i)
+        {
+            int nextIndex = (i + 1) % holesDug.Count;
+            boundaryPoints += GCD(Math.Abs(holesDug[i].X - holesDug[nextIndex].X), Math.Abs(holesDug[i].Y - holesDug[nextIndex].Y));
+        }
+
+        // Calculate the number of lattice points strictly inside the polygon using Pick's Theorem
+        long interiorPoints = CalculateInteriorPoints(holesDug);
+
+        // Calculate the area using Pick's Theorem
+        holeArea = interiorPoints + boundaryPoints / 2 - 1;  
+
+        Console.WriteLine($"Hole area: {holeArea}");
+    }
+
+    // Helper method to calculate the greatest common divisor (GCD) using Euclidean algorithm
+    private static long GCD(long a, long b)
+    {
+        while (b != 0)
+        {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    // Helper method to calculate the number of lattice points strictly inside the polygon
+    private static long CalculateInteriorPoints(List<(int X, int Y)> polygon)
+    {
+        long interiorPoints = 0;
+
+        // Calculate twice the signed area of the polygon
+        for (int i = 0; i < polygon.Count; ++i)
+        {
+            int nextIndex = (i + 1) % polygon.Count;
+            interiorPoints += (polygon[i].X + polygon[nextIndex].X) * (polygon[i].Y - polygon[nextIndex].Y);
+        }
+
+        // Take the absolute value and divide by 2 to get the signed area
+        long signedArea = Math.Abs(interiorPoints) / 2;
+
+        // Calculate the number of lattice points strictly inside the polygon
+        return signedArea - polygon.Count + 1;
+    }
+}
