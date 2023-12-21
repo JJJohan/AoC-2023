5fc00d87b979cedb07841d8742b30e3ad6ad7a27
Johan Rensenbrink <admin@renscreations.com>
2023-12-21 19:15:35 +1000

    Stole the solution for day 21 part 2, sorry.. I did simplify it a bit.

--- Projects/Day 21/Program.cs
+++ Projects/Day 21/Program.cs
@@ -6,7 +6,6 @@ static class Program
     {
         string[] lines = File.ReadAllLines(Path.Combine("..", "Input", "day21.txt"));
 
-
         int cols = lines[0].Length;
         int rows = lines.Length;
 
@@ -17,10 +16,10 @@ static class Program
             string line = lines[i];
             for (int j = 0; j < line.Length; ++j)
             {
-                char c = line[j];
-                grid[j, i] = c;
+                char letter = line[j];
+                grid[j, i] = letter;
 
-                if (c == 'S') start = (j, i);
+                if (letter == 'S') start = (j, i);
             }
         }
 
@@ -69,90 +68,61 @@ static class Program
 
         // Part 2
 
-        //steps = 26501365;
-        steps = 1000;
+        Debug.Assert(rows == cols);
+        long grids = 26501365 / rows;
+        long rem = 26501365 % rows;
 
-        Dictionary<(int x, int y), int>[] optionSets = [new(), new()];
-        Dictionary<(int x, int y), int> firstSet = optionSets[0];
-        for (int y = 0; y < rows; ++y)
+        // I stole this quadratic logic and solver from the AoC sub-Reddit so I don't really deserve the star,
+        // but I spend quite a long time trying to figure out a way to map it and brute forcing it so eh.
+        List<int> sequence = new();
+        HashSet<(int x, int y)> work = new() { start };
+        steps = 0;
+        (int x, int y)[] dirs = new (int, int)[4];
+        for (int n = 0; n < 3; n++)
         {
-            for (int x = 0; x < cols; ++x)
+            long end = n * rows + rem;
+            while (steps < end)
             {
-                firstSet[(x, y)] = 0;
-            }
-        }
-        firstSet[start] = 1;
-
-        int setIndex = 0;
-        while (steps > 0)
-        {
-            --steps;
-
-           // float percent = (float)(26501365 - steps) / 26501365 * 100.0f;
-            //if (steps % 100 == 0) Console.WriteLine($"Steps to go: {steps} ({percent:0.00}%)");
-
-            Dictionary<(int x, int y), int> set = optionSets[setIndex];
-            Dictionary<(int x, int y), int> next = optionSets[(setIndex + 1) % optionSets.Length];
-            foreach ((int x, int y) in set.Keys)
-            {
-                next[(x, y)] = 0;
-            }
-
-            foreach (KeyValuePair<(int, int), int> pair in set)
-            {
-                (int x, int y)  = pair.Key;
-                int current = pair.Value;
-                Debug.Assert(current >= 0);
-                if (current == 0) continue;
-
-                int xMin1Abs = x - 1;
-                int yMin1Abs = y - 1;
-                int xPlus1Abs = x + 1;
-                int yPlus1Abs = y + 1;
-                int xMin1 = x - 1;
-                while (xMin1 < 0) xMin1 += cols;
-                int xPlus1 = (x + 1) % cols;
-
-                int yMin1 = y - 1;
-                while (yMin1 < 0) yMin1 += rows;
-                int yPlus1 = (y + 1) % rows;
-
-                if (grid[xMin1, y] != '#')
+                HashSet<(int x, int y)> next = new();
+                foreach ((int x, int y) in work)
                 {
-                    next[(xMin1, y)] = set[(xMin1, y)] + 1;
+                    dirs[0] = (x - 1, y);
+                    dirs[1] = (x + 1, y);
+                    dirs[2] = (x, y - 1);
+                    dirs[3] = (x, y + 1);
+
+                    for (int i = 0; i < 4; ++i)
+                    {
+                        (int x, int y) dir = dirs[i];
+
+                        (int x, int y) coord = (dir.x % cols, dir.y % rows);
+                        while (coord.x < 0) coord.x += cols;
+                        while (coord.y < 0) coord.y += rows;
+                        
+                        if (grid[coord.x, coord.y] != '#')
+                        {
+                            next.Add((dir.x, dir.y));
+                        }
+                    }
                 }
 
-                if (grid[xPlus1, y] != '#')
-                {
-                    next[(xPlus1, y)] = set[(xPlus1, y)] + 1;
-                }
-
-                if (grid[x, yMin1] != '#')
-                {
-                    next[(x, yMin1)] = set[(x, yMin1)] + 1;
-                }
-
-                if (grid[x, yPlus1] != '#')
-                {
-                    next[(x, yPlus1)] = set[(x, yPlus1)] + 1;
-                }
+                work = next;
+                ++steps;
             }
 
-            foreach ((int x, int y) in next.Keys)
-            {
-                int current = set[(x, y)];
-                if (current > 0)
-                    set[(x, y)] = current - 1;
-            }
-
-            setIndex = (setIndex + 1) % optionSets.Length;
+            sequence.Add(work.Count);
         }
 
-        long availableTileCount = 0;
-        foreach (int count in optionSets[setIndex].Values)
-        {
-            availableTileCount += count;
-        }
+        // Solve for the quadratic coefficients
+        // ax^2 + bx + c
+        int c = sequence[0];
+        int aPlusB = sequence[1] - c;
+        int fourAPlusTwoB = sequence[2] - c;
+        int twoA = fourAPlusTwoB - (2 * aPlusB);
+        int a = twoA / 2;
+        int b = aPlusB - a;
+
+        long availableTileCount = a * (grids * grids) + b * grids + c;
 
         Console.WriteLine($"Available tiles: {availableTileCount}");
     }
